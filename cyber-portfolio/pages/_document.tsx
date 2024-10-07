ca3fc3712b7d7ff60ba4adf48ff1f211611df3fe
btakiso <122041628+btakiso@users.noreply.github.com>
2024-10-07 12:00:11 -0400

    all platform favicon ico implemented

--- cyber-portfolio/pages/_document.tsx
+++ cyber-portfolio/pages/_document.tsx
@@ -4,7 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Bereket" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <Main />
