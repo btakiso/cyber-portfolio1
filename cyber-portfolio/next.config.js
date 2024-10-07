7cd18ba6590db8b644b49e05b0c7c4d32f452cc4
btakiso <122041628+btakiso@users.noreply.github.com>
2024-10-07 13:59:55 -0400

    Update next.config.js

--- cyber-portfolio/next.config.js
+++ cyber-portfolio/next.config.js
@@ -7,7 +7,20 @@ const nextConfig = {
                 headers: [
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https: http:;"
+                        value: `
+                            default-src 'self';
+                            script-src 'self' 'unsafe-eval' 'unsafe-inline';
+                            style-src 'self' 'unsafe-inline';
+                            img-src 'self' data: blob:;
+                            font-src 'self';
+                            object-src 'none';
+                            base-uri 'self';
+                            form-action 'self';
+                            frame-ancestors 'none';
+                            frame-src 'self' https://www.youtube.com;
+                            connect-src 'self' https://api.bereketakiso.com https://bereketakiso.com https://www.bereketakiso.com https://cyber-portfolio-72310aa69f55.herokuapp.com;
+                            upgrade-insecure-requests;
+                        `.replace(/\s{2,}/g, ' ').trim()
                     },
                 ],
             },
