3e1f1174b6a0cbfe50924f42570b198f5425e4a3
btakiso <122041628+btakiso@users.noreply.github.com>
2024-11-15 16:17:53 -0500

    Update blog-page.tsx

--- cyber-portfolio/app/components/blog-page.tsx
+++ cyber-portfolio/app/components/blog-page.tsx
@@ -125,8 +125,8 @@ export function BlogPage() {
             {/* Featured Blog Post */}
             {currentPage === 1 && filteredPosts.length > 0 && (
               <div className="mb-12 bg-gray-900 rounded-xl overflow-hidden shadow-lg">
-                <div className="flex flex-col md:flex-row">
-                  <div className="w-full md:w-[600px] relative h-[300px] md:h-[400px]">
+                <div className="md:flex h-[400px]">
+                  <div className="md:w-1/2 relative">
                     {filteredPosts[0].attributes.image && filteredPosts[0].attributes.image.data ? (
                       <Image
                         src={prependApiUrl(filteredPosts[0].attributes.image.data.attributes.url)}
@@ -150,7 +150,7 @@ export function BlogPage() {
                       />
                     )}
                   </div>
-                  <div className="flex-1 p-6 md:p-8 bg-gray-900">
+                  <div className="md:w-1/2 p-6 md:p-8 bg-gray-900 flex flex-col justify-center">
                     <div className="uppercase tracking-wide text-sm text-blue-500 font-semibold">
                       {filteredPosts[0].attributes.Category}
                     </div>
@@ -181,7 +181,7 @@ export function BlogPage() {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
               {currentPosts.map((post) => (
                 <div key={post.id} className="bg-gray-900 rounded-xl overflow-hidden shadow-lg transition-transform duration-300 hover:-translate-y-1">
-                  <div className="relative h-[220px]">
+                  <div className="relative h-[240px]">
                     {post.attributes.image && post.attributes.image.data ? (
                       <Image
                         src={prependApiUrl(post.attributes.image.data.attributes.url)}
@@ -204,7 +204,7 @@ export function BlogPage() {
                       />
                     )}
                   </div>
-                  <div className="p-6">
+                  <div className="p-6 bg-gray-900">
                     <div className="uppercase tracking-wide text-sm text-blue-500 font-semibold mb-1">
                       {post.attributes.Category}
                     </div>
